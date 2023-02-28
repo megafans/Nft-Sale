3478970b76e6a8ba2bfe22e530643d81516e246c
Bartek <bartek.codes@gmail.com>
2023-02-28 22:21:38 +0100

    feat: cc button

--- src/components/organisms/profile.tsx
+++ src/components/organisms/profile.tsx
@@ -2,6 +2,9 @@ import { ArrowLongRightIcon } from '@heroicons/react/24/solid'
 import { Suspense, useState } from 'react'
 import { useAccount } from 'wagmi'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
+import WertWidget from '@wert-io/widget-initializer'
+import { signSmartContractData } from '@wert-io/widget-sc-signer'
+import { v4 as uuid } from 'uuid'
 
 import { Button, Modal, NftList, ProfileBanner, ProfileEdit, Spinner } from '@/components'
 import { useBuyNFT, useMounted, useUser } from '@/hooks'
@@ -14,7 +17,7 @@ export const Profile = () => {
   const mounted = useMounted()
   const { openConnectModal } = useConnectModal()
 
-  const { isConnected } = useAccount({
+  const { isConnected, address } = useAccount({
     onConnect({ address, isReconnected }) {
       !isReconnected && sendUserWallet(address)
     },
@@ -39,12 +42,42 @@ export const Profile = () => {
     }
   }
 
+  const signedData = signSmartContractData(
+    {
+      address: address!,
+      commodity: 'ETH',
+      commodity_amount: 0.000000005,
+      pk_id: 'key1',
+      sc_address: '0x0F006A670651F0A5d1677b680ad8Aa0b72FE60B0',
+      sc_id: uuid(),
+      sc_input_data: '0c1a3f6ff5436d7412a02b198e52fa0b677f8aec99fae81f38a78ba03631ecaf',
+    },
+    '603e3dcaa6c6dc3361b5efa2dcce346407739bb2fcb624b371d13e8aafae81f1'
+  )
+
+  const wertWidget = new WertWidget({
+    ...signedData,
+    partner_id: '01GKW611J71EF5B8H9MS00G6M4',
+    container_id: 'wert-widget',
+    click_id: uuid(),
+    commodities: 'ETH:Ethereum-Goerli,MATIC:Polygon',
+    lang: 'en',
+    origin: 'https://sandbox.wert.io',
+    theme: 'dark',
+    autosize: true,
+  })
+
   return (
     <>
       {getProfileBannerView()}
       <div className="flex flex-col md:flex-row items-center md:justify-evenly mt-14 space-y-8 md:space-y-0">
         <Button type="button" size="lg" variant="primary" onClick={connected ? () => buyNFT() : openConnectModal}>
           Buy NFT with ETH
+          <ArrowLongRightIcon className="w-6 h-6 ml-10" />
+        </Button>
+
+        <Button type="button" size="lg" variant="primary" onClick={() => wertWidget.open()}>
+          Buy NFT with CC
           <ArrowLongRightIcon className="w-6 h-6 ml-10" />
         </Button>
       </div>
@@ -64,6 +97,7 @@ export const Profile = () => {
           <p className="text-2xl font-bold mt-20 text-white text-center">Please connect your wallet to see your NFTs</p>
         )}
       </div>
+      <div id="wert-widget" />
     </>
   )
 }
