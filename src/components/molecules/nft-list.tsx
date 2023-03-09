316299789bdcf08cf3d063cdf9c939193f6a530d
Bartek <bartek.codes@gmail.com>
2023-03-09 14:46:40 +0100

    refactor: RWD fixes on nft list

--- src/components/molecules/nft-list.tsx
+++ src/components/molecules/nft-list.tsx
@@ -81,7 +81,7 @@ export const NftList = () => {
   return (
     <ul
       role="list"
-      className="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+      className="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
     >
       {(nftIds ? (nftIds as BigNumber[]) : []).map(nft => {
         const nftId = nft.toString()
