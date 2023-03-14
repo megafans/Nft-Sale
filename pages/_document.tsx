232c57113655b51321fb1e71bdb641924451e5cb
Bartek <bartek.codes@gmail.com>
2023-03-14 23:09:49 +0100

    fix: fix linters

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export const Document = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap" rel="stylesheet" />
+    </Head>
     <body>
       <Main />
       <NextScript />
