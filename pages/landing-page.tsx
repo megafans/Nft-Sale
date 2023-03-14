1250a2eacf7fcadad6ef042c259e1d95213e89b9
Bartek <bartek.codes@gmail.com>
2023-03-15 00:40:41 +0100

    feat: more animations

--- pages/landing-page.tsx
+++ pages/landing-page.tsx
@@ -150,102 +150,110 @@ const Header = () => (
 )
 
 const Hero = () => (
-  <div
-    className="h-screen overflow-x-hidden z-0"
-    style={{
-      position: 'relative',
-      flexBasis: 'auto',
-      flexGrow: 0,
-      flexShrink: 1,
-      height: 'clamp(60vh, 56vw, 100vh)',
-    }}
-  >
+  <AnimatePresence>
     <div
+      className="h-screen overflow-x-hidden z-0"
       style={{
         position: 'relative',
-        display: 'flex',
-        minHeight: '1px',
-        alignSelf: 'auto',
-        height: '100%',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
+        flexBasis: 'auto',
+        flexGrow: 0,
+        flexShrink: 1,
+        height: 'clamp(60vh, 56vw, 100vh)',
       }}
     >
       <div
         style={{
           position: 'relative',
-          alignContent: 'flex-start',
-          flexWrap: 'wrap',
-          paddingBottom: 'clamp(50px, 8vw, 170px)',
-          lineHeight: 1,
-          width: 'max-content',
-          zIndex: 0,
+          display: 'flex',
+          minHeight: '1px',
+          alignSelf: 'auto',
+          height: '100%',
+          alignItems: 'flex-end',
+          justifyContent: 'center',
         }}
       >
         <div
           style={{
             position: 'relative',
+            alignContent: 'flex-start',
+            flexWrap: 'wrap',
+            paddingBottom: 'clamp(50px, 8vw, 170px)',
+            lineHeight: 1,
+            width: 'max-content',
+            zIndex: 0,
           }}
         >
-          <p
+          <motion.div
+            initial={{ scale: 1.2, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            whileHover={{ scale: 1.2 }}
+            exit={{ scale: 1.2, opacity: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: 0.4 }}
             style={{
-              color: '#C7FFFD',
-              fontSize: 'clamp(24px, 3.6vw, 40px)',
-              fontWeight: 600,
-              fontFamily: '"Space Grotesk", sans-serif;',
-              lineHeight: 1,
-              padding: '36px 144px',
-              textShadow: '0px 0px 9px rgba(199, 255, 253, 0.5)',
+              position: 'relative',
             }}
           >
-            NFT COLLECTION
-          </p>
+            <p
+              style={{
+                color: '#C7FFFD',
+                fontSize: 'clamp(24px, 3.6vw, 40px)',
+                fontWeight: 600,
+                fontFamily: '"Space Grotesk", sans-serif;',
+                lineHeight: 1,
+                padding: '36px 144px',
+                textShadow: '0px 0px 9px rgba(199, 255, 253, 0.5)',
+              }}
+            >
+              NFT COLLECTION
+            </p>
 
-          <Image
-            fill
-            src={bannerButton}
-            className="-z-10"
-            alt="nft-collection-button-background"
-            style={{ objectFit: 'contain' }}
-          />
-        </div>
+            <Image
+              fill
+              src={bannerButton}
+              className="-z-10"
+              alt="nft-collection-button-background"
+              style={{ objectFit: 'contain' }}
+            />
+          </motion.div>
 
-        <div
-          style={{
-            textAlign: 'center',
-            display: 'block',
-            position: 'relative',
-          }}
-        >
-          <p
+          <div
             style={{
-              padding: 24,
+              textAlign: 'center',
+              display: 'block',
+              position: 'relative',
             }}
           >
-            STAKE TO EARN
-          </p>
-          <Image
-            fill
-            src={lightButton}
-            className="-z-10"
-            alt="nft-collection-light-button-background"
-            style={{ objectFit: 'contain' }}
-          />
+            <p
+              style={{
+                padding: 24,
+              }}
+            >
+              STAKE TO EARN
+            </p>
+            <Image
+              fill
+              src={lightButton}
+              className="-z-10"
+              alt="nft-collection-light-button-background"
+              style={{ objectFit: 'contain' }}
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    <Image
-      src={heroCharacters}
-      priority
-      fill
-      alt="hero-characters"
-      className="-z-10 max-md:object-cover object-contain object-center min-w"
-      // style={{ minWidth: 700 }}
-      placeholder="blur"
-    />
-    <Image src={heroBackground} priority fill alt="hero-background" className=" -z-20 object-cover object-bottom" />
-  </div>
+      <Image
+        src={heroCharacters}
+        priority
+        fill
+        alt="hero-characters"
+        className="-z-10 max-md:object-cover object-contain object-center min-w"
+        // style={{ minWidth: 700 }}
+        placeholder="blur"
+      />
+      <Image src={heroBackground} priority fill alt="hero-background" className=" -z-20 object-cover object-bottom" />
+    </div>
+  </AnimatePresence>
 )
 
 const NftMintSection = () => (
@@ -411,6 +419,7 @@ const TeamSection = () => (
       <AnimatePresence>
         <div className="flex justify-center gap-2">
           <motion.a
+            whileHover={{ scale: 1.1 }}
             initial={{ x: -300, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             exit={{ x: -300, opacity: 0 }}
@@ -424,11 +433,12 @@ const TeamSection = () => (
             <Image src={teamJeff} alt="" />
           </motion.a>
           <motion.a
+            whileHover={{ scale: 1.1 }}
             initial={{ x: 300, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             exit={{ x: 300, opacity: 0 }}
             viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
             href="https://www.linkedin.com/in/colinbracey/"
             target="_blank"
             className="mr-auto cursor-pointer"
@@ -632,33 +642,40 @@ const NftsSection = () => {
 }
 
 const TournamentSection = () => (
-  <div
-    className="py-24"
-    style={{
-      background: 'linear-gradient(180deg, #232B50 0%, #19132A 100%)',
-    }}
-  >
-    <div className="flex flex-col gap-20 px-5">
-      <h2
-        className="text-center text-5xl leading-none font-semibold"
-        style={{
-          minWidth: 240,
-          color: '#B7FFFD',
-          textShadow: '0px 0px 9px rgba(199, 255, 253, 0.5)',
-        }}
-      >
-        NOVEL GAMIFIED NFT <br />
-        TOURNAMENT SYSTEM
-      </h2>
-      <div
-        style={{
-          background: 'radial-gradient(at center center, #0015FF 0%, #FFFFFF00 74%)',
-        }}
-      >
-        <Image src={tournament} width={tournament.width} alt="tournament-graph" />
+  <AnimatePresence>
+    <motion.div
+      initial={{ y: -300, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      exit={{ y: 300, opacity: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay: 0.6 }}
+      className="py-24"
+      style={{
+        background: 'linear-gradient(180deg, #232B50 0%, #19132A 100%)',
+      }}
+    >
+      <div className="flex flex-col gap-20 px-5">
+        <h2
+          className="text-center text-5xl leading-none font-semibold"
+          style={{
+            minWidth: 240,
+            color: '#B7FFFD',
+            textShadow: '0px 0px 9px rgba(199, 255, 253, 0.5)',
+          }}
+        >
+          NOVEL GAMIFIED NFT <br />
+          TOURNAMENT SYSTEM
+        </h2>
+        <div
+          style={{
+            background: 'radial-gradient(at center center, #0015FF 0%, #FFFFFF00 74%)',
+          }}
+        >
+          <Image src={tournament} width={tournament.width} alt="tournament-graph" />
+        </div>
       </div>
-    </div>
-  </div>
+    </motion.div>
+  </AnimatePresence>
 )
 
 const StatsSection = () => {
