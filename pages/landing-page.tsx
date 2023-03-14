eac234826556c434f1990293b70d6b10179eb55d
Bartek <bartek.codes@gmail.com>
2023-03-15 00:26:08 +0100

    feat: parallax animation

--- pages/landing-page.tsx
+++ pages/landing-page.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import clsx from 'clsx'
 import Image from 'next/image'
-import { AnimatePresence, motion } from 'framer-motion'
+import { useLayoutEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion, useScroll, useTransform } from 'framer-motion'
 
 import { Container, Copyright, Logo, PaymentButtons } from '@/components'
 
@@ -44,6 +45,46 @@ import nftMiddle from '../public/landing/nft-2.webp'
 import nftRight from '../public/landing/nft-3.webp'
 
 import { NextPageWithLayout } from './_app'
+
+type ParallaxProps = {
+  children: JSX.Element
+  offset?: number
+}
+
+const Parallax = ({ children, offset = 50 }: ParallaxProps): JSX.Element => {
+  const [elementTop, setElementTop] = useState(0)
+  const [clientHeight, setClientHeight] = useState(0)
+  const ref = useRef<HTMLDivElement>(null)
+
+  const { scrollY } = useScroll()
+
+  // start animating our element when we've scrolled it into view
+  const initial = elementTop - clientHeight
+  // end our animation when we've scrolled the offset specified
+  const final = elementTop + offset
+
+  const x = useTransform(scrollY, [initial, final], [offset, -offset])
+
+  useLayoutEffect(() => {
+    const element = ref.current
+
+    const onResize = () => {
+      if (element) {
+        setElementTop(element.getBoundingClientRect().top + window.scrollY || window.pageYOffset)
+        setClientHeight(window.innerHeight)
+      }
+    }
+    onResize()
+    window.addEventListener('resize', onResize)
+    return () => window.removeEventListener('resize', onResize)
+  }, [ref])
+
+  return (
+    <motion.div ref={ref} style={{ x }}>
+      {children}
+    </motion.div>
+  )
+}
 
 const Header = () => (
   <div
@@ -208,138 +249,127 @@ const Hero = () => (
 )
 
 const NftMintSection = () => (
-  <AnimatePresence>
-    <motion.div
-      initial={{ y: 300, opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
-      exit={{ y: -300, opacity: 0 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.5, delay: 0.6 }}
+  <div
+    style={{
+      height: '916px',
+      marginBottom: 50,
+      backgroundImage: 'radial-gradient(at center right, rgba(80, 40, 101, 0.5) 0%, #16131E 68%)',
+      backgroundColor: '#16131E',
+      opacity: 1,
+      transition: 'background 0.3s, border-radius 0.3s, opacity 0.3s;',
+    }}
+  >
+    <div className="flex flex-col pt-24">
+      <div style={{ marginBottom: '20px' }}>
+        <p
+          className="text-center"
+          style={{
+            color: '#C7FFFD',
+            fontSize: '48px',
+            fontWeight: 600,
+            lineHeight: '48px',
+            fontFamily: '"Space Grotesk", sans-serif;',
+          }}
+        >
+          NFT MINT
+        </p>
+      </div>
+      <div style={{ marginBottom: '20px' }}>
+        <p
+          className="text-center"
+          style={{
+            color: '#C7FFFD',
+            fontSize: '32px',
+            fontWeight: 600,
+            lineHeight: '32px',
+            fontFamily: '"Space Grotesk", sans-serif;',
+          }}
+        >
+          ETHEREUM
+        </p>
+      </div>
+      <div>
+        <p
+          className="text-center"
+          style={{
+            color: '#C7FFFD',
+            fontSize: '18px',
+            fontWeight: 600,
+            lineHeight: '33px',
+            fontFamily: '"Space Grotesk", sans-serif;',
+          }}
+        >
+          Available To Open Sea
+        </p>
+      </div>
+    </div>
+
+    <div className="flex mix-blend-lighten align-middle justify-center relative px-44">
+      <div className="relative z-0 mb-8">
+        <div className="-left-48 absolute -z-10 object-contain top-1/2 -translate-y-1/2 max-lg:-left-36">
+          <Parallax offset={75}>
+            <Image src={'/landing/leftgun.webp'} height={186} width={208} alt="gun-left" />
+          </Parallax>
+        </div>
+
+        <Image
+          src={'/landing/ethereum.webp'}
+          height={481}
+          width={650}
+          style={{
+            objectFit: 'contain',
+          }}
+          alt="eth-logo"
+        />
+        <div className="-right-48 absolute -z-10 object-contain top-1/2 -translate-y-1/2 max-lg:-right-36">
+          <Parallax offset={-75}>
+            <Image src={'/landing/rightgun.webp'} height={186} width={208} alt="gun-left" />
+          </Parallax>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="flex justify-center gap-6 max-w-screen-md mx-auto text-center max-sm:flex-wrap"
       style={{
-        height: '916px',
-        marginBottom: 50,
-        backgroundImage: 'radial-gradient(at center right, rgba(80, 40, 101, 0.5) 0%, #16131E 68%)',
-        backgroundColor: '#16131E',
-        opacity: 1,
-        transition: 'background 0.3s, border-radius 0.3s, opacity 0.3s;',
+        color: '#B7FFFD',
       }}
     >
-      <div className="flex flex-col pt-24">
-        <div style={{ marginBottom: '20px' }}>
-          <p
-            className="text-center"
-            style={{
-              color: '#C7FFFD',
-              fontSize: '48px',
-              fontWeight: 600,
-              lineHeight: '48px',
-              fontFamily: '"Space Grotesk", sans-serif;',
-            }}
-          >
-            NFT MINT
-          </p>
-        </div>
-        <div style={{ marginBottom: '20px' }}>
-          <p
-            className="text-center"
-            style={{
-              color: '#C7FFFD',
-              fontSize: '32px',
-              fontWeight: 600,
-              lineHeight: '32px',
-              fontFamily: '"Space Grotesk", sans-serif;',
-            }}
-          >
-            ETHEREUM
-          </p>
-        </div>
-        <div>
-          <p
-            className="text-center"
-            style={{
-              color: '#C7FFFD',
-              fontSize: '18px',
-              fontWeight: 600,
-              lineHeight: '33px',
-              fontFamily: '"Space Grotesk", sans-serif;',
-            }}
-          >
-            Available To Open Sea
-          </p>
-        </div>
+      <div className="flex max-sm:basis-full relative z-0 basis-1/2" style={{ maxWidth: 400 }}>
+        <a
+          href="https://megafans.io/wp-content/uploads/2022/06/MS-MegaFans-White-Paper-May-24-2022.pdf"
+          target={'_blank'}
+          className="w-full px-14 py-8"
+          style={{
+            minWidth: 240,
+            textAlign: 'center',
+            fontSize: '1.3111111111111rem',
+            lineHeight: 0,
+          }}
+        >
+          LIGHT PAPER
+        </a>
+        <Image src={headerButton} className="-z-10" fill alt="header logo" />
       </div>
 
-      <div className="flex mix-blend-lighten align-middle justify-center relative px-44">
-        <div className="relative z-0 mb-8">
-          <Image
-            src={'/landing/leftgun.webp'}
-            height={186}
-            width={208}
-            alt="gun-left"
-            className="-left-48 absolute -z-10 object-contain top-1/2 -translate-y-1/2 max-lg:-left-36"
-          />
-
-          <Image
-            src={'/landing/ethereum.webp'}
-            height={481}
-            width={650}
-            style={{
-              objectFit: 'contain',
-            }}
-            alt="eth-logo"
-          />
-          <Image
-            src={'/landing/rightgun.webp'}
-            height={186}
-            width={208}
-            alt="gun-left"
-            className="-right-48 absolute -z-10 object-contain top-1/2 -translate-y-1/2 max-lg:-right-36"
-          />
-        </div>
+      <div className="flex  max-sm:basis-full relative z-0 basis-1/2" style={{ maxWidth: 400 }}>
+        <a
+          href="https://www.premint.xyz/MegaFans/"
+          target={'_blank'}
+          className="w-full px-14 py-8"
+          style={{
+            minWidth: 240,
+            textAlign: 'center',
+            fontSize: '1.3111111111111rem',
+            lineHeight: 0,
+          }}
+        >
+          LAUNCH March 15
+        </a>
+        <Image src={headerButton} className="-z-10" fill alt="header logo" />
       </div>
-
-      <div
-        className="flex justify-center gap-6 max-w-screen-md mx-auto text-center max-sm:flex-wrap"
-        style={{
-          color: '#B7FFFD',
-        }}
-      >
-        <div className="flex max-sm:basis-full relative z-0 basis-1/2" style={{ maxWidth: 400 }}>
-          <a
-            href="https://megafans.io/wp-content/uploads/2022/06/MS-MegaFans-White-Paper-May-24-2022.pdf"
-            target={'_blank'}
-            className="w-full px-14 py-8"
-            style={{
-              minWidth: 240,
-              textAlign: 'center',
-              fontSize: '1.3111111111111rem',
-              lineHeight: 0,
-            }}
-          >
-            LIGHT PAPER
-          </a>
-          <Image src={headerButton} className="-z-10" fill alt="header logo" />
-        </div>
-
-        <div className="flex  max-sm:basis-full relative z-0 basis-1/2" style={{ maxWidth: 400 }}>
-          <a
-            href="https://www.premint.xyz/MegaFans/"
-            target={'_blank'}
-            className="w-full px-14 py-8"
-            style={{
-              minWidth: 240,
-              textAlign: 'center',
-              fontSize: '1.3111111111111rem',
-              lineHeight: 0,
-            }}
-          >
-            LAUNCH March 15
-          </a>
-          <Image src={headerButton} className="-z-10" fill alt="header logo" />
-        </div>
-      </div>
-    </motion.div>
-  </AnimatePresence>
+    </div>
+  </div>
 )
 
 const TeamSection = () => (
