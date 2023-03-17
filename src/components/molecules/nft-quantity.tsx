7eed5b9e63b35e5b4aafb26cac1b46448eac4d4d
Bartek <bartek.codes@gmail.com>
2023-03-17 23:28:46 +0100

    refactor: buy more than 7 NFT's using CC

--- src/components/molecules/nft-quantity.tsx
+++ src/components/molecules/nft-quantity.tsx
@@ -1,35 +1,21 @@
-import { RadioGroup } from '@headlessui/react'
-import cslx from 'clsx'
-
 type NFTQuantityProps = {
   nftQuantity: string
   setNftQuantity: (value: string) => void
 }
 
-const radioOptions = ['1', '2', '3', '4', '5', '6', '7']
-
 export const NFTQuantity = ({ nftQuantity, setNftQuantity }: NFTQuantityProps) => {
   return (
     <div className="flex">
-      <RadioGroup value={nftQuantity} onChange={setNftQuantity} className="flex flex-col mt-4 w-full justify-between">
-        <RadioGroup.Label className="text-white">Check how many nfts you want to buy:</RadioGroup.Label>
-        <div className="flex">
-          {radioOptions.map(option => (
-            <RadioGroup.Option value={option} key={option}>
-              {({ checked }) => (
-                <span
-                  className={cslx(
-                    'text-white rounded-full w-10 h-10 flex items-center justify-center text-xl font-bold cursor-pointer',
-                    checked && 'border border-white text-current'
-                  )}
-                >
-                  {option}
-                </span>
-              )}
-            </RadioGroup.Option>
-          ))}
-        </div>
-      </RadioGroup>
+      <div className="flex flex-col mt-4 w-full justify-between">
+        <p className="text-white text-center">Check how many nfts you want to buy:</p>
+        <input
+          className="h-16 w-24 bg-white/10 text-center mt-2 text-3xl font-bold text-white m-auto"
+          onChange={e => setNftQuantity(e.target.value)}
+          value={nftQuantity}
+          type="number"
+          min="1"
+        />
+      </div>
     </div>
   )
 }
