8e569c69486773dbba67399f0431664334b79e2d
Bartek <bartek.codes@gmail.com>
2023-03-22 22:11:02 +0100

    feat: image load on single nft and confirmation pages

--- src/components/molecules/congratulations.tsx
+++ src/components/molecules/congratulations.tsx
@@ -1,52 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
-import axios from 'axios'
 import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/24/solid'
 import Confetti from 'react-confetti'
 import { useWindowSize } from 'react-use'
-import { useContractRead } from 'wagmi'
-import { useState, useEffect } from 'react'
 
 import { ButtonLink } from '@/components'
 import { useBuyNFT, useMounted } from '@/hooks'
-import { nftSmartContractAddress } from '@/helpers/constants'
-import { ensRegistryABI } from '@/utils/abi'
 
-import type { NFTPayload } from '@/types/nft'
-
-const fetchNFTListData = async (url: string) => {
-  const response = await axios.get(url as string)
-  const { data } = response
-  return data
-}
-
-const baseContract: any = {
-  address: nftSmartContractAddress,
-  abi: ensRegistryABI,
-  chainId: 5,
-}
+const CID = 'QmP5ehuisBJFomF5ZBrML2CeJiGZADFtvb2iGtXe5NqVbd'
 
 export const Congratulations = () => {
   const { nftIds } = useBuyNFT()
   const nftId = nftIds?.slice(-1)
-  const [nft, setData] = useState<NFTPayload>()
-  const { data: nftPayload } = useContractRead<any, any, any>({
-    ...baseContract,
-    functionName: 'tokenURI',
-    args: [nftId ? +nftId?.toString() : ''],
-  })
 
   const mounted = useMounted()
   const { width, height } = useWindowSize()
 
-  useEffect(() => {
-    if (nftPayload) {
-      fetchNFTListData(nftPayload).then(setData)
-    }
-  }, [nftPayload])
-
-  return nft ? (
+  return nftId && mounted ? (
     <>
-      {width && mounted && <Confetti width={width} height={height} numberOfPieces={100} tweenDuration={10000} />}
+      {width && <Confetti width={width} height={height} numberOfPieces={100} tweenDuration={10000} />}
       <ButtonLink href="/profile" variant="transparent" size="lg" ribbon>
         <ArrowLongLeftIcon className="w-6 h-6 mr-10" />
         <span className="font-bold">Back to profile</span>
@@ -65,12 +36,12 @@ export const Congratulations = () => {
       <div className="flex justify-center pt-20">
         <img
           className="aspect-[1/1] w-80 rounded-2xl object-cover"
-          src={`https://ipfs.io/ipfs${nft?.image?.replace('ipfs:/', '')}`}
+          src={`https://megafans.mypinata.cloud/ipfs/${CID}/${nftId.toString()}.png`}
           alt="NFT"
         />
       </div>
-      <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-white text-center">{nft?.name}</h3>
-      <p className="text-base leading-7 text-white text-center">{nft?.description}</p>
+      <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-white text-center">{nftId.toString()}</h3>
+      <p className="text-base leading-7 text-white text-center">Megafans Gamer Girlz</p>
     </>
   ) : null
 }
--- src/components/molecules/nft-details.tsx
+++ src/components/molecules/nft-details.tsx
@@ -2,58 +2,32 @@
 import { ArrowLongLeftIcon } from '@heroicons/react/24/solid'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { BigNumber } from 'ethers'
-import axios from 'axios'
-import { useContractRead } from 'wagmi'
 
 import { ButtonLink, NftRewardsList, Spinner } from '@/components'
 import { useBrowser, useBuyNFT, useMounted } from '@/hooks'
 import { fetcher } from '@/utils/fetcher'
 import { api } from '@/helpers/api'
-import { nftSmartContractAddress } from '@/helpers/constants'
-import { ensRegistryABI } from '@/utils/abi'
-
-const fetchNFTListData = async (url: string) => {
-  const response = await axios.get(url as string)
-  const { data } = response
-  return data
-}
-
-const baseContract: any = {
-  address: nftSmartContractAddress,
-  abi: ensRegistryABI,
-  chainId: 5,
-}
 
 export const NftDetailsEntity = ({ nftId }: { nftId: any }) => {
   const isBrowser = useBrowser()
   const token = isBrowser ? localStorage.getItem('token') : null
   const { query } = useRouter()
   const mounted = useMounted()
-  const [nft, setData] = useState<any>(null)
-  const { data: nftPayload } = useContractRead<any, any, any>({
-    ...baseContract,
-    functionName: 'tokenURI',
-    args: [+nftId.toString()],
-  })
-
-  useEffect(() => {
-    if (nftPayload) {
-      fetchNFTListData(nftPayload).then(setData)
-    }
-  }, [nftPayload])
 
   const { data, error, isLoading } = useSWR(
     token ? [`${api.URL}api/NFT/ListTotalNFTRewards?nftId=${query.id}`, token] : null,
     ([url, token]) => fetcher(url, token)
   )
 
+  const CID = 'QmP5ehuisBJFomF5ZBrML2CeJiGZADFtvb2iGtXe5NqVbd'
+
   if (error) {
     return <p className="text-center text-white">Failed to load</p>
   }
 
-  return data && nft ? (
+  return data && nftId ? (
     <>
       <ButtonLink href="/profile" variant="transparent" size="lg" ribbon>
         <ArrowLongLeftIcon className="w-6 h-6 mr-10" />
@@ -64,14 +38,14 @@ export const NftDetailsEntity = ({ nftId }: { nftId: any }) => {
         <div className="w-1/2">
           <img
             className="aspect-[1/1] w-full rounded-2xl object-cover md:-rotate-6"
-            src={`https://ipfs.io/ipfs${nft?.image.replace('ipfs:/', '')}`}
-            alt=""
+            src={`https://megafans.mypinata.cloud/ipfs/${CID}/${nftId}.png`}
+            alt={nftId}
           />
         </div>
         {!isLoading && mounted && (
           <div className="w-1/2 flex flex-col text-center items-end justify-start space-y-5">
-            <h3 className="text-lg font-semibold tracking-tight text-white">{nft?.name}</h3>
-            <p className="text-base leading-7 text-white">{nft?.description}</p>
+            <h3 className="text-lg font-semibold tracking-tight text-white">{nftId}</h3>
+            <p className="text-base leading-7 text-white">Megafans Gamer Girlz</p>
             <p className="text-base leading-7 text-white">value: {data?.dollarValue}</p>
             <p className="text-base leading-7 text-white">MFAN tokens won: {data?.totalRewards}</p>
           </div>
@@ -92,7 +66,7 @@ export const NFTDetails = () => {
   )
   return nftId ? (
     <>
-      <NftDetailsEntity nftId={nftId} />
+      <NftDetailsEntity nftId={nftId.toString()} />
       <NftRewardsList nftId={query.id} />
     </>
   ) : null
