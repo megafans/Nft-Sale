ce1594e99ad8077ae7bcf16f72ed8a42107f4366
Bartek <bartek.codes@gmail.com>
2023-03-28 20:53:23 +0200

    feat: estimated price for cc payment

--- src/components/molecules/payment-modals.tsx
+++ src/components/molecules/payment-modals.tsx
@@ -59,6 +59,13 @@ export const PaymentModals = ({
         <>
           {wertWidgetStep === 1 && (
             <div className="flex items-center flex-col space-y-4 pt-10">
+              <p className="text-xl font-bold text-white mb-4">
+                You are going to pay
+                <span className="bg-purple p-4 h-4 w-4 rounded-full mx-2">
+                  {(0.005 * Number(nftQuantity)).toFixed(3)}
+                </span>
+                ETH (around {(usd * 0.005 * Number(nftQuantity)).toFixed(2)} USD)
+              </p>
               <span className="text-white">Choose NFT quantity:</span>
               <input
                 className="h-20 w-32 bg-white/10 text-center mt-2 text-3xl font-bold text-white"
