d55498852c883d370f9bcc7d38a4e9e967468dfc
Bartek <bartek.codes@gmail.com>
2023-03-28 21:49:42 +0200

    refactor: price conversion component

--- src/components/molecules/payment-modals.tsx
+++ src/components/molecules/payment-modals.tsx
@@ -3,7 +3,7 @@ import { useAccount } from 'wagmi'
 import { AnimatePresence, motion } from 'framer-motion'
 import { ArrowLongRightIcon } from '@heroicons/react/24/solid'
 
-import { Button, Modal } from '@/components'
+import { Button, Modal, PriceConversion } from '@/components'
 import { useBuyNFT, useCurrency, useWertPayment } from '@/hooks'
 
 type PaymentModalsProps = {
@@ -59,13 +59,7 @@ export const PaymentModals = ({
         <>
           {wertWidgetStep === 1 && (
             <div className="flex items-center flex-col space-y-4 pt-10">
-              <p className="text-xl font-bold text-white mb-4">
-                You are going to pay
-                <span className="bg-purple p-4 h-4 w-4 rounded-full mx-2">
-                  {(0.005 * Number(nftQuantity)).toFixed(3)}
-                </span>
-                ETH (around {(usd * 0.005 * Number(nftQuantity)).toFixed(2)} USD)
-              </p>
+              <PriceConversion currency={usd} nftQuantity={nftQuantity} />
               <span className="text-white">Choose NFT quantity:</span>
               <input
                 className="h-20 w-32 bg-white/10 text-center mt-2 text-3xl font-bold text-white"
@@ -97,11 +91,7 @@ export const PaymentModals = ({
       </Modal>
       <Modal open={paymentModalVisibility} onClose={() => onModalClose(paymentModalClose)} title="Buy NFT">
         <div className="flex flex-col items-center justify-center space-y-6 mt-10">
-          <p className="text-xl font-bold text-white">
-            You are going to pay
-            <span className="bg-purple p-4 h-4 w-4 rounded-full mx-2">{(0.005 * Number(nftQuantity)).toFixed(3)}</span>
-            ETH (around {(usd * 0.005 * Number(nftQuantity)).toFixed(2)} USD)
-          </p>
+          <PriceConversion currency={usd} nftQuantity={nftQuantity} />
           <div className="flex items-center space-x-3">
             <span className="text-white">Choose NFT quantity:</span>
             <input
