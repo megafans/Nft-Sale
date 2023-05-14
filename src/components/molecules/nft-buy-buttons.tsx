397a57489fe26c616b7d3ce84875f4f33a81258d
Bartek <bartek.codes@gmail.com>
2023-05-14 22:21:13 +0200

    refactor: hide sale buttons when contract is paused

--- src/components/molecules/nft-buy-buttons.tsx
+++ src/components/molecules/nft-buy-buttons.tsx
@@ -13,20 +13,30 @@ export const NftBuyButtons = ({ onETHPaymentClick, onCCPaymentClick }: NftBuyBut
   const { isPaused } = useBuyNFT()
 
   return (
-    <div className="flex flex-col md:flex-row items-start md:justify-evenly mt-20 space-y-8 md:space-y-0">
-      <div>
-        <Button type="button" size="lg" variant="primary" onClick={onETHPaymentClick} disabled={isPaused}>
-          Buy NFT with ETH
-          <ArrowLongRightIcon className="w-6 h-6 ml-10" />
-        </Button>
-      </div>
+    <>
+      {isPaused ? (
+        <div className="flex flex-col md:flex-row items-start md:justify-evenly mt-4 space-y-8 md:space-y-0">
+          <p className="text-2xl font-bold mt-20 text-white text-center max-w-3xl">
+            The NFT sale is not currently open
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col md:flex-row items-start md:justify-evenly mt-20 space-y-8 md:space-y-0">
+          <div>
+            <Button type="button" size="lg" variant="primary" onClick={onETHPaymentClick} disabled={isPaused}>
+              Buy NFT with ETH
+              <ArrowLongRightIcon className="w-6 h-6 ml-10" />
+            </Button>
+          </div>
 
-      <div className="-mt-10">
-        <Button type="button" size="lg" variant="primary" onClick={onCCPaymentClick} disabled={isPaused}>
-          Buy NFT with CC
-          <ArrowLongRightIcon className="w-6 h-6 ml-10" />
-        </Button>
-      </div>
-    </div>
+          <div className="-mt-10">
+            <Button type="button" size="lg" variant="primary" onClick={onCCPaymentClick} disabled={isPaused}>
+              Buy NFT with CC
+              <ArrowLongRightIcon className="w-6 h-6 ml-10" />
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
