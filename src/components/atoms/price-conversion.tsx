117ef262db6fdede3392a9eb5ea11cfc5aa4666d
Bartek <bartek.codes@gmail.com>
2023-06-18 21:44:07 +0200

    refactor: formated price

--- src/components/atoms/price-conversion.tsx
+++ src/components/atoms/price-conversion.tsx
@@ -16,9 +16,9 @@ export const PriceConversion = ({ currency, nftQuantity }: PriceConnversionProps
       You are going to pay
       <span className="bg-purple p-4 h-4 w-4 rounded-full mx-2">{formatedPrice}</span>
       ETH (
-      {Number(currency * Number(price) * Number(nftQuantity)) < 1
+      {Number(currency * Number(formatedPrice) * Number(nftQuantity)) < 1
         ? 'below 1 USD'
-        : `around ${currency * Number(formatedPrice) * Number(nftQuantity)} USD`}
+        : `around ${numbro(currency * Number(formatedPrice) * Number(nftQuantity)).format({ mantissa: 3 })} USD`}
       )
     </p>
   )
